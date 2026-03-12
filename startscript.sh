32945e13e8efd6ea13c89397a957172e88c72512
Elliott-17 <elliotthipkiss@gmail.com>
2026-03-12 14:48:04 +0000

    Fix install script

--- startscript.sh
+++ startscript.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
+# curl -O https://ekh9.teaching.cs.st-andrews.ac.uk/rustworkshop/startscript.sh >/dev/null 2>&1; bash startscript.sh
 
-cd ~/Desktop
+echo "Setting up the rust workshop!!! Please Wait"
+echo "All files will be saved in the Desktop Folder, so if you want to keep anything, make sure to grab it"
+
+
+user=$(whoami)
+mkdir /home/$user/Desktop >/dev/null 2>&1
+cd /home/$user/Desktop >/dev/null 2>&1
 
 
 set -e
@@ -18,30 +25,27 @@ fi
 if [ "$performinstall" = true ]; then
     echo "Installing Rustlings..."
     cargo install rustlings >/dev/null 2>&1
+
     user=$(whoami)
     export PATH="/home/$user/.cargo/bin:$PATH"
-    echo "Installed"
+    echo "Done"
 fi
 
 
 ### Check if in git tree
 if git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
-    echo "Already inside a git repository."
     performclone=false
-else
-    echo "Not inside a git repository."
 fi
 
 ### Check if already cloned
 if [ -d "RustWorkshop" ]; then
-    echo "RustWorkshop directory already exists."
     performclone=false
 fi
 
 ### If not cloned and we arent in a git tree
 ### Then Clone it
 if [ "$performclone" = true ]; then
-    echo "Cloning Rustlings workshop..."
+    echo "Cloning workshop..."
     git clone https://github.com/Elliott-17/RustWorkshop RustWorkshop >/dev/null 2>&1
 fi
 
